c9a186545b020e64bf251bce382cf51fc5ed006e
Farshad <farshadzargary1997@gmail.com>
2021-08-08 11:46:37 +0430

    gallery added to property

--- property/views.py
+++ property/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Property, Category
-from django.db.models import F
+from django.db.models import Avg, Max, Min
 from django.views.generic import (
     ListView,
     DetailView,
@@ -13,14 +13,9 @@ from django.views.generic import (
 def Home_Page(request):
     properties = Property.objects.all().order_by('-pub_date')[:6] # get last 6 items by publish date
     categories = Category.objects.all()
-    location = request.GET.get('location')
-    category = request.GET.get('category')
-    look_for = request.GET.get('look_for')
     context = {
         "properties": properties,
-        "location": location,
         "categories": categories,
-        "look_for": look_for,
     }
     return render(request, 'property/index.html', context)
 
@@ -31,11 +26,31 @@ class PropertiesList(ListView):
     ordering = ['-pub_date']
     paginate_by = 6
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_context_data(self, *args, **kwargs):
+        context = super(PropertiesList, self).get_context_data(*args, **kwargs)
         context['popular_properties'] = Property.objects.order_by('-views')[:3]
         context['categories'] = Category.objects.all()
+        context['min_max_price'] = Property.objects.aggregate(maximum = Max('price'), minimum = Min('price'))
+        context['current_order'] = self.request.GET.get('sort_by')
+        context['current_pagination'] = self.get_paginate_by(self.paginate_by)
         return context
+    
+    def get_paginate_by(self, queryset):
+        if self.request.GET.get("paginate_by") == "":
+            return self.paginate_by
+        return self.request.GET.get("paginate_by", self.paginate_by) 
+    
+    def get_ordering(self):
+        ordering = super(PropertiesList, self).get_ordering()
+        if self.request.GET.get('sort_by') == "Name":
+            self.get_paginate_by(self.paginate_by)
+            return ('-title')
+        elif self.request.GET.get('sort_by') == "Price":
+            return ('-price')
+        elif self.request.GET.get('sort_by') == "Date":
+            return ('-pub_date')
+        else:
+            return self.ordering
  
     
 class PropertyDetailView(DetailView):
@@ -63,11 +78,30 @@ class SearchView(ListView):
     ordering = ['-pub_date']
     paginate_by = 6
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchView, self).get_context_data(*args, **kwargs)
         context['popular_properties'] = Property.objects.order_by('-views')[:3]
         context['categories'] = Category.objects.all()
+        context['current_order'] = self.request.GET.get('sort_by')
+        context['current_pagination'] = self.get_paginate_by(self.paginate_by)
         return context
+    
+    def get_paginate_by(self, queryset):
+        if self.request.GET.get("paginate_by") == "":
+            return self.paginate_by
+        return self.request.GET.get("paginate_by", self.paginate_by) 
+    
+    def get_ordering(self):
+        ordering = super(SearchView, self).get_ordering()
+        if self.request.GET.get('sort_by') == "Name":
+            self.get_paginate_by(self.paginate_by)
+            return ('-title')
+        elif self.request.GET.get('sort_by') == "Price":
+            return ('-price')
+        elif self.request.GET.get('sort_by') == "Date":
+            return ('-pub_date')
+        else:
+            return self.ordering
     
     def get_queryset(self):
         location = self.request.GET.get('location')
@@ -90,3 +124,15 @@ class SearchView(ListView):
         else:
             object_list = Property.objects.all()
         return object_list
+    
+class GalleryView(ListView):
+    model = Property
+    template_name = 'property/gallery.html'
+    context_object_name = 'property'
+    paginate_by = 12
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(GalleryView, self).get_context_data(*args, **kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+        
