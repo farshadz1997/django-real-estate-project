2872d7a8d324dea55ae4c4a6a54eae31f0146f9d
Farshad <farshadzargary1997@gmail.com>
2021-08-16 20:02:49 +0430

    Search view added in blogs

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("blogs", views.BlogList.as_view(), name="Blogs-List"),
     path("blogs/<int:pk>", views.BlogDetail.as_view(), name="Blog-Detail"),
-    path("blogs/search", views.BlogList.as_view(), name="Blog-Search"),
+    path("blogs/search", views.BlogSearch.as_view(), name="Blog-Search"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,15 @@
 from django.db.models.query import QuerySet
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
-from . import models
+from .models import Blog, Category, Comments, Tag
 from django.views.generic.edit import FormMixin
 from .forms import CommentForm
 from django.views.generic import ListView, DetailView
-from django.db.models import Count
+from django.db.models import Count, Q
 
 class BlogList(ListView):
-    model = models.Blog
+    model = Blog
     template_name = 'blog/blog_list.html'
     context_object_name = 'blogs'
     ordering = ['-pub_date']
@@ -17,42 +17,26 @@ class BlogList(ListView):
     
     def get_context_data(self, *args, **kwargs):
         context = super(BlogList, self).get_context_data(*args, **kwargs)
-        context['categories'] = models.Category.objects.all().annotate(num_blogs=Count('blog'))
-        context['tags'] = models.Tag.objects.all()
-        context['recent_posts'] = models.Blog.objects.order_by('-pub_date')[:3]
+        context['categories'] = Category.objects.all().annotate(num_blogs=Count('blog'))
+        context['tags'] = Tag.objects.all()
+        context['recent_posts'] = Blog.objects.order_by('-pub_date')[:3]
         return context
     
-    def get_queryset(self):
-        search_for = self.request.GET.get('search_for')
-        if search_for:
-            query_set = models.Blog.objects.filter(title__icontains = search_for, description__icontains = search_for, content__icontains = search_for)
-        else:
-            query_set = models.Blog.objects.all()
-        return query_set
-    
 class BlogDetail(FormMixin,DetailView):
-    model = models.Blog
+    model = Blog
     template_name = 'blog/blog_detail.html'
     context_object_name = 'blog'
     form_class = CommentForm
     
     def get_context_data(self, *args, **kwargs):
         context = super(BlogDetail, self).get_context_data(*args, **kwargs)
-        context['categories'] = models.Category.objects.all().annotate(num_blogs=Count('blog'))
-        context['tags'] = models.Tag.objects.all()
-        context['recent_posts'] = models.Blog.objects.order_by('-pub_date').exclude(id=self.object.pk)[:3]
-        context['related_posts'] = models.Blog.objects.all().filter(category = self.object.category).exclude(id=self.object.pk)[:2]
-        context['comments'] = models.Comments.objects.all().filter(blog = self.object)
+        context['categories'] = Category.objects.all().annotate(num_blogs=Count('blog'))
+        context['tags'] = Tag.objects.all()
+        context['recent_posts'] = Blog.objects.order_by('-pub_date').exclude(id=self.object.pk)[:3]
+        context['related_posts'] = Blog.objects.all().filter(category = self.object.category).exclude(id=self.object.pk)[:2]
+        context['comments'] = Comments.objects.all().filter(blog = self.object)
         context['form'] = CommentForm()
         return context
-    
-    def get_queryset(self):
-        search_for = self.request.GET.get('search_for')
-        if search_for:
-            query_set = models.Blog.objects.filter(title__icontains = search_for, description__icontains = search_for, content__icontains = search_for)
-        else:
-            query_set = models.Blog.objects.all()
-        return query_set
     
     def post(self, request, *args, **kwargs):
         form = CommentForm(request.POST)
@@ -70,3 +54,25 @@ class BlogDetail(FormMixin,DetailView):
             context = self.get_context_data(*args, **kwargs)
             context['form'] = CommentForm()
             return self.render_to_response(context=context)
+        
+class BlogSearch(ListView):
+    model = Blog
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'blogs'
+    ordering = ['-pub_date']
+    paginate_by = 8
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(BlogSearch, self).get_context_data(*args, **kwargs)
+        context['categories'] = Category.objects.all().annotate(num_blogs=Count('blog'))
+        context['tags'] = Tag.objects.all()
+        context['recent_posts'] = Blog.objects.order_by('-pub_date')[:3]
+        return context
+      
+    def get_queryset(self):
+        search_for = self.request.GET.get('q')
+        if search_for:
+            query = Blog.objects.filter(Q(title__icontains=search_for) | Q(content__icontains=search_for))
+        else:
+            query = Blog.objects.all()
+        return query
