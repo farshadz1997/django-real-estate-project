cffe8f2ff98e002ce49ac3dfafa9f379278906d0
Malo BRIAND <malo.briand@nikon.com>
2023-06-01 17:58:24 +0100

    yes 2

--- home_property_project/settings.py
+++ home_property_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k+9qr&evs)gantx7iw!(n66#d^8m5s3sxakao18f7m7u*1o9pj"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
