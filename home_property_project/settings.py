d25e04fb8067a7c88de6db0141f5b46151a00707
Malo BRIAND <malo.briand@nikon.com>
2023-06-01 18:11:41 +0100

    yes 3

--- home_property_project/settings.py
+++ home_property_project/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
